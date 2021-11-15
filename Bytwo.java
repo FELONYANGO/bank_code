2618aa4db5a45f0216717b7ef473007e5fee96d5
FELONYANGO <felixonyango@gmail.com>
2021-11-15 22:54:32 +0300

    second commit

--- Bytwo.java
+++ Bytwo.java
@@ -1,11 +1,12 @@
-public class Bytwo implements Series{
+public class Bytwo implements Series {
     int val;
-     public void bytwos() {
-         val = 0;
-     }
 
-    public  int getnext(){
-          val+=2;
-          return val;
+    public void bytwos() {
+        val = 0;
+    }
+
+    public int getnext() {
+        val += 2;
+        return val;
     }
 }
